ead3850afc2ab2d963f6be99b9d7dff6063c80c9
fatusu <fatema.tama@gmail.com>
2016-06-15 05:08:52 +0600

    First Lecture complete

---
+++ src/SecondClass.java
@@ -0,0 +1,24 @@
+
+public class SecondClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		short firstShortNumber;
+		int secondIntNumber;
+		float myFloatNumber;
+		double myDoubleNumber;
+		
+		firstShortNumber=10;
+		secondIntNumber=20;
+		myFloatNumber=10;
+		myDoubleNumber=10;
+		
+		double result=myDoubleNumber/3; 
+		
+		System.out.println(firstShortNumber);
+		System.out.println(secondIntNumber);
+		System.out.println(result);
+
+	}
+
+}
---
+++ src/StartPoint.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class StartPoint {
+	static Scanner sc=new Scanner(System.in); 
+
+	public static void main(String[] args) {
+		System.out.println("Please write you name\n");
+		String name=sc.nextLine();
+		// TODO Auto-generated method stub
+		System.out.println("Hello"+" "+name);
+	}
+
+}
